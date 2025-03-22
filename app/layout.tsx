f93b6f009abaedeaa55243b48782ac6024a8d3b8
salma-soussi <soussiselma@hotmail.com>
2025-03-22 18:47:00 -0500

    Setup

---
+++ app/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from "next";
+import { Mona_Sans } from "next/font/google";
+import "./globals.css";
+
+const monaSans = Mona_Sans({
+  variable: "--font-mona-sans",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "PrepProAI",
+  description:
+    "PrepProAI is an AI-powered interview prep platform offering mock interviews, real-time feedback, and expert-curated questions to help you land your dream job.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang='en' className='dark'>
+      <body className={`${monaSans.className} antialiased`}>{children}</body>
+    </html>
+  );
+}
